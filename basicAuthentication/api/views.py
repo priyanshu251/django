e8f6e3c987a27aec64081445abad592eb1caf21a
priyanshu251 <iampriyanshu251@gmail.com>
2024-01-25 01:15:20 +0530

    basic authentication

---
+++ basicAuthentication/api/views.py
@@ -0,0 +1,17 @@
+from .models import Student 
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+# this is used when only list and retrieve action are to be performed (eg : corona virus website)
+    
+# class StudentReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer

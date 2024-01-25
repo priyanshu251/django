9b694d6933a5b850fa38f5d302224ab52487cf64
priyanshu251 <iampriyanshu251@gmail.com>
2024-01-25 12:53:41 +0530

    last commit

--- basicAuthentication/api/views.py
+++ basicAuthentication/api/views.py
@@ -2,13 +2,14 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAuthenticated] # isse ye hoga ki agar banda authenticated hai to allow ho jaega doesnt matter ki wo staff hai ya nhi
+    permission_classes = [IsAdminUser]
 
 # this is used when only list and retrieve action are to be performed (eg : corona virus website)
     

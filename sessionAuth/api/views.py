889309d4e6fdb3ebf0a62edb07375e7d4c64dd85
priyanshu251 <iampriyanshu251@gmail.com>
2024-01-28 01:30:41 +0530

    new commit

--- sessionAuth/api/views.py
+++ sessionAuth/api/views.py
@@ -2,7 +2,7 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
@@ -12,6 +12,8 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     # permission_classes = [IsAdminUser] # agar banda isstaff hua tabhi auth ho paega 
     # permission_classes = [AllowAny]
     permission_classes = [IsAuthenticatedOrReadOnly] # anyone can read but only authorised users can write 
+    # permission_classes = [DjangoModelPermissions] # isme aisa hoga ki jo bhi perms dena hai wo django administration me mention kr do particular user ko to w perms us user ko mil jaega 
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
 # this is used when only list and retrieve action are to be performed (eg : corona virus website)
     

604e126811590d5873f6377f18b9fae3f85f9cbe
priyanshu251 <iampriyanshu251@gmail.com>
2024-01-29 02:04:08 +0530

    new commit

--- sessionAuth/sessionAuth/urls.py
+++ sessionAuth/sessionAuth/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from api import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token #restframework me authtoken me views file hai jisme obtain_auth_token ek function hoga uska kaam hai token generate krna 
 
 # creating router object
 router = DefaultRouter()
@@ -13,6 +14,9 @@ router.register('studentapi', views.StudentModelViewSet,basename='student')
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include(router.urls)),
-    path("auth/",include("rest_framework.urls",namespace="rest_framework"))
+    path("auth/",include("rest_framework.urls",namespace="rest_framework")),
+    path("gettoken/",obtain_auth_token) # ye as function view hai isliye bas aise hi likh diye agar ye class view hota to "obtain_auth_token.asView()" likhte
+    #gettoken ki jagah naam kuch bhi likh skte hai, iss url pe hit hone pe obtain_auth_token function hit hoga joki authtoken k view me hai 
+    
 ]
 
--- tokenAuth/api/views.py
+++ tokenAuth/api/views.py
@@ -1,19 +1,19 @@
 from .models import Student 
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    # authentication_classes = [TokenAuthentication]
+    authentication_classes = [SessionAuthentication]
     # permission_classes = [IsAuthenticated] # isse ye hoga ki agar banda authenticated hai to allow ho jaega doesnt matter ki wo staff hai ya nhi
     # permission_classes = [IsAdminUser] # agar banda isstaff hua tabhi auth ho paega 
     # permission_classes = [AllowAny]
-    # permission_classes = [IsAuthenticatedOrReadOnly] # anyone can read but only authorised users can write 
+    permission_classes = [IsAuthenticatedOrReadOnly] # anyone can read but only authorised users can write 
     # permission_classes = [DjangoModelPermissions] # isme aisa hoga ki jo bhi perms dena hai wo django administration me mention kr do particular user ko to w perms us user ko mil jaega 
-    # permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
 # this is used when only list and retrieve action are to be performed (eg : corona virus website)
     

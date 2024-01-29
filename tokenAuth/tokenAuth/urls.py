5fcbec1d1e939b271d6ac6e24c2994a861f45450
priyanshu251 <iampriyanshu251@gmail.com>
2024-01-30 00:54:19 +0530

    new commit

--- tokenAuth/tokenAuth/urls.py
+++ tokenAuth/tokenAuth/urls.py
@@ -1,22 +1,22 @@
-"""
-URL configuration for tokenAuth project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api import views
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token #restframework me authtoken me views file hai jisme obtain_auth_token ek function hoga uska kaam hai token generate krna 
+
+# creating router object
+router = DefaultRouter()
+
+#register StudentModelViewSet in router 
+router.register('studentapi', views.StudentModelViewSet,basename='student')
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",include(router.urls)),
+    path("auth/",include("rest_framework.urls",namespace="rest_framework")),
+    path("gettoken/",obtain_auth_token) # ye as function view hai isliye bas aise hi likh diye agar ye class view hota to "obtain_auth_token.asView()" likhte
+    #gettoken ki jagah naam kuch bhi likh skte hai, iss url pe hit hone pe obtain_auth_token function hit hoga joki authtoken k view me hai 
+    
 ]
+
